914a00d402263212bc2dabec8f3a0e3642bb1b03
JoaquinF <joaquinpfelix@gmail.com>
2026-03-08 21:15:47 -0700

    Check 5

--- 210-Lab-19.cpp
+++ 210-Lab-19.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 const string REVIEW_FILE = "reviews.txt";
@@ -33,9 +35,47 @@ public:
     string getTitle() const;
 };
 
+// Function Prototypes
+double generateRandomRating();
+vector<string>loadCommentsFromFile();
+
 int main()
 {
+    srand(static_cast<unsigned>(time(0)));
 
+    vector<string> comments = loadCommentsFromFile();
+
+    if (comments.size() < NUM_MOVIES * REVIEWS_PER_MOVIE)
+    {
+        cout << "ERROR: Not enough comments in file.\n";
+        return 0;
+    }
+
+    vector<Movie> movies;
+
+    movies.push_back(Movie("Inception"));
+    movies.push_back(Movie("The Matrix"));
+    movies.push_back(Movie("Interstellar"));
+    movies.push_back(Movie("The Godfather"));
+
+    int commentIndex = 0;
+
+    for (int i = 0; i < NUM_MOVIES; i++)
+    {
+        for (int j = 0; j < REVIEWS_PER_MOVIE; j++)
+        {
+            double rating = generateRandomRating();
+            movies[i].addReview(rating, comments[commentIndex]);
+            commentIndex++;
+        }
+    }
+
+    for (int i = 0; i < NUM_MOVIES; i++)
+    {
+        movies[i].outputReviews();
+    }
+    
+    return 0;
 }
 
 // Movie constructor initializes members
@@ -118,4 +158,32 @@ double genereateRandomRating()
 {
     int randomValue = rand() %
         (MAX_RANDOM - MIN_RANDOM + 1)
+        + MIN_RANDOM;
+    return randomValue / 10.0;
+}
+
+// loadCommentsFromFile() reads comments
+// arguments: none
+// returns: vector of strings
+vector<string> loadCommentsFromFile()
+{
+    vector<string> comments;
+    ifstream infile(REVIEW_FILE);
+
+    if (infile)
+    {
+        cout << "Error: could not open"
+             << REVIEW_FILE << endl;
+        return comments;
+    }
+
+    string line;
+
+    while (getline(infile, line))
+    {
+        comments.push_back(line);
+    }
+
+    infile.close();
+    return comments;
 }
