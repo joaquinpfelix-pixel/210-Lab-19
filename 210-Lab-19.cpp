16d1ea5f2ccf4531c4c837f98fda6f101894348a
JoaquinF <joaquinpfelix@gmail.com>
2026-03-08 17:42:01 -0700

    Check 2

--- 210-Lab-19.cpp
+++ 210-Lab-19.cpp
@@ -6,6 +6,27 @@
 
 using namespace std;
 
+class Movie {
+private:
+    struct ReviewNode 
+    {
+        double rating;
+        string comment;
+        ReviewNode* next;
+    };
+
+    string title;
+    ReviewNode* head;
+
+public:
+    Movie(string t);
+    ~Movie();
+
+    void addReview(double rating, const string& comment);
+    void outputReviews() const;
+    string getTitle() const;
+};
+
 int main()
 {
 
